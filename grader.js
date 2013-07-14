070aadaafdc01ac2f25bf44d4becd9490bddd743
Al Gorhythm <gorhythm.al@gmail.com>
2013-07-14 07:18:11 +0000

    Updated grader with async fork

--- grader.js
+++ grader.js
@@ -17,21 +17,25 @@ var assertFileExists = function(infile) {
 
 assertFileExists('grader.js')
 
-var fileDownloader = function(localFile)
+var fileDownloader = function(remoteUrl, localFile, cb, cbparam)
 {
-    var restlerHtmlFile = function(urlParam) {
-	if (urlParam)
-	{
-	    console.error('Trying to get url %s', urlParam);
-	    rest.get(urlParam).on('complete', function(result, response) 
-		    {
-			console.error('Downloaded url %s successfully', urlParam);
-			fs.writeFileSync(localFile, result);
-		    });
-	} 
-	return urlParam;
-    };
-    return restlerHtmlFile;
+    if (fs.existsSync(localFile)) { fs.unlinkSync(localFile); }
+    if (remoteUrl)
+    {
+	console.error('Trying to get url %s', remoteUrl);
+	var res = rest.get(remoteUrl).on('complete', function(result, response) 
+		{
+		    console.error('Downloaded url %s successfully', remoteUrl);
+		    fs.writeFileSync(localFile, result);
+		    cb(cbparam);
+		});
+	//console.error('Status of request: %s', res.request.finished.toString());
+	//console.error('Headers of request: %s', JSON.stringify(res.url, null, 4));
+	/*
+	   var res = rest.get(remoteUrl);
+	   fs.writeFileSync(localFile, res.request.res.rawEncoded.toString());
+	   */
+    }
 };
 
 var cheerioHtmlFile = function(htmlFile) {
@@ -58,22 +62,43 @@ var clone = function(fn)
     return fn.bind({});
 };
 
-var pass = function() {}
+var pass = function(invar) { return invar; }
+
+var main_fork = function(prg)
+{
+    assertFileExists(prg.file);
+    var checkJson = checkHtmlFile(prg.checks, prg.file);
+    //var checkJson = checkHtmlFile(CHECKSFILE_DEFAULT, HTMLFILE_DEFAULT);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson)
+}
 
 if (require.main == module)
 {
     program
 	.option('-c, --checks <check_file>', 'path to checks json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-u, --url <url>', 'url to html file', fileDownloader(HTMLFILE_DEFAULT), '')
-	.option('-f, --file <html_file>', 'path to html file', pass, HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'url to html file', clone(pass), '')
+	.option('-f, --file <html_file>', 'path to html file', clone(pass), HTMLFILE_DEFAULT)
 	.parse(process.argv);
-    assertFileExists(HTMLFILE_DEFAULT);
-    var checkJson = checkHtmlFile(program.checks, program.file);
-    //var checkJson = checkHtmlFile(CHECKSFILE_DEFAULT, HTMLFILE_DEFAULT);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson)
+    if (program.url) {
+	program.file = HTMLFILE_DEFAULT; 
+	fileDownloader(program.url, program.file, main_fork, program);
+    } else {
+	main_fork(program);
+    }
 }
 else
 {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+/*
+var rest = require('restler');
+var res = rest.get('http://www.google.com');
+while (!res.request.finished) {}
+var str = res.request.res.rawEncoded;
+console.log(str)
+console.log(res.request.res.rawEncoded.substr(0, 120));
+localFile = 'index.html_tocheck';
+fs.writeFileSync(localFile, res.request.res.rawEncoded.toString());
+ */
