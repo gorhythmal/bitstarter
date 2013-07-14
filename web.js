38c14323fe1821282b0178ad37b897f8b854f9bc
Al Gorhythm <gorhythm.al@gmail.com>
2013-07-14 03:38:08 +0000

    Message from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('oye varta, tere kitne nambar aaye??!');
+	var msgbuff = fs.readFileSync("index.html");
+	response.send(msgbuff.toString());
 });
 
 var port = process.env.PORT || 5000;
